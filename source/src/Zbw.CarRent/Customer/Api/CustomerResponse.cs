f6f9a3c83a6393e3b389f05e58755c621124bcef
Patrick Nef <patricknef@outlook.com>
2023-06-19 20:01:59 +0200

    add customer controller with customer response dto

---
+++ source/src/Zbw.CarRent/Customer/Api/CustomerResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Zbw.CarRent.Customer.Api {
+  public class CustomerResponse {
+
+    public Guid Id { get; set; }
+
+    public string CustomerNumber { get; set; }
+
+    public string Name { get; set; }
+
+    public string? Address { get; set; }
+
+  }
+}
